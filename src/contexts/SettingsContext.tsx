88eaac29511879602efa3527582b085bf7888e66
arsmoriendy <nugrahasteven@gmail.com>
2025-01-13 20:17:29 +0700

    feat: settings parser

--- src/contexts/SettingsContext.tsx
+++ src/contexts/SettingsContext.tsx
@@ -1,30 +1,46 @@
 import { createContext, createEffect, JSX } from "solid-js";
 import { createStore } from "solid-js/store";
+import { z } from "zod";
+import wrapError from "../lib/wrapError";
+import zsodm from "../lib/zsodm";
 
-export type Settings = {
-  colorFormat: "hsl" | "rgb" | "hex";
-  showTable: {
-    dark: boolean;
-    light: boolean;
-  };
-  roundFloats: boolean;
-};
+const SettingsSchema = z
+  .object({
+    colorFormat: z.enum(["hsl", "rgb", "hex"]),
+    showTable: z.object({
+      dark: z.boolean(),
+      light: z.boolean(),
+    }),
+    roundFloats: z.boolean(),
+  })
+  .default({
+    colorFormat: "hsl",
+    showTable: {
+      dark: true,
+      light: true,
+    },
+    roundFloats: false,
+  });
+
+export type Settings = z.infer<typeof SettingsSchema>;
 
 const lsKey = "gruvboxCheatsheetSettings";
-const lsSettings = localStorage[lsKey];
 
-const defaultSettings: Settings = lsSettings
-  ? JSON.parse(lsSettings)
-  : {
-      colorFormat: "hsl",
-      showTable: {
-        dark: true,
-        light: true,
-      },
-      roundFloats: false,
-    };
+const parseLs = (): Settings => {
+  const lsStr = localStorage[lsKey];
+  const defaultSettings = SettingsSchema._def.defaultValue();
 
-const settingsStore = createStore(defaultSettings);
+  var { success, data: lsObj } = wrapError(() => JSON.parse(lsStr));
+  if (!success) return defaultSettings;
+
+  var { success, data } = SettingsSchema.safeParse(lsObj);
+  if (success) return data!;
+
+  return zsodm(lsObj, SettingsSchema) as Settings;
+};
+
+const parsedLs = parseLs();
+const settingsStore = createStore(parsedLs);
 
 createEffect(() => {
   localStorage[lsKey] = JSON.stringify(settingsStore[0]);
