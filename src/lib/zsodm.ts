1044d848279a8fed6aa6de3bd1d80ef8a2e0893c
arsmoriendy <nugrahasteven@gmail.com>
2025-01-13 17:26:36 +0700

    feat: zsodm object merger

---
+++ src/lib/zsodm.ts
@@ -0,0 +1,48 @@
+import { z } from "zod";
+
+const zsodm = <S extends z.SomeZodObject>(
+  srcObj: { [k: string]: any },
+  defObj: z.infer<S>,
+  schema: S,
+): S => {
+  const retObj: { [k: string]: any } = {};
+
+  // iterate through schema keys and types
+  Object.entries(schema.shape).forEach(([k, zType]) => {
+    zType = zType;
+    const srcVal = srcObj[k];
+
+    // recursively run this function on object types
+    if (zType instanceof z.ZodObject) {
+      retObj[k] = zsodm(srcVal, defObj[k], zType);
+      return;
+    }
+
+    const { success } = zType.safeParse(srcVal);
+    if (success) {
+      retObj[k] = srcVal;
+      return;
+    }
+
+    retObj[k] = defObj[k];
+    return;
+  });
+
+  return retObj as S;
+};
+
+/**
+ * Zod Safe Object Deep Merge
+ *
+ * *Safely* deep merges an object with a source object that has a zod schema. By safe meaning:
+ * - Mismatching value types (determined using the source object's zod schema) will be replaced with the source value
+ * - Object entries (key value paris) not fonud in the source object will be discarded
+ */
+const wrapped = <S extends z.SomeZodObject, D extends z.ZodDefault<S>>(
+  srcObj: {
+    [k: string]: any;
+  },
+  schema: D,
+): S => zsodm(srcObj, schema._def.defaultValue(), schema._def.innerType);
+
+export default wrapped;
