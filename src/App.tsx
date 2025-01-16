1496783e215ded2afc02059e9269228e19b7af6a
arsmoriendy <nugrahasteven@gmail.com>
2025-01-16 20:32:19 +0700

    feat: github + issues hrefs

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { SettingsContextProvider } from "./contexts/SettingsContext";
 import { ColorTables } from "./components/fragments/ColorTables";
 import Settings from "./components/fragments/Settings";
 import { Toaster } from "./components/elements/toast";
+import { Button } from "./components/elements/button";
+import Bug from "lucide-solid/icons/bug";
+import Github from "lucide-solid/icons/github";
 
 const App: Component = () => {
   return (
@@ -10,7 +13,25 @@ const App: Component = () => {
       <div class="flex flex-col max-w-2xl xl:max-w-7xl relative left-1/2 -translate-x-1/2 px-2">
         <header class="flex items-center justify-between my-2">
           <h1 class="font-bold text-xl">Gruvbox Cheatsheet</h1>
-          <Settings />
+          <div class="flex gap-2">
+            <Button
+              size="icon"
+              variant="outline"
+              as="a"
+              href="https://github.com/arsmoriendy/gruvbox-cheatsheet"
+            >
+              <Github />
+            </Button>
+            <Button
+              size="icon"
+              variant="outline"
+              as="a"
+              href="https://github.com/arsmoriendy/gruvbox-cheatsheet/issues"
+            >
+              <Bug />
+            </Button>
+            <Settings />
+          </div>
         </header>
         <ColorTables />
         <Toaster />
