e7c1c236d6b5fe311c1cc457bef6882dc2d274f1
arsmoriendy <nugrahasteven@gmail.com>
2025-01-19 17:03:19 +0700

    refactor: refine repoitem props

--- src/components/fragments/GithubDropdown.tsx
+++ src/components/fragments/GithubDropdown.tsx
@@ -10,7 +10,10 @@ import {
 import { GithubButton } from "./GithubButton";
 
 export const GithubDropdown = () => {
-  const repos = ["arsmoriendy/gruvbox-cheatsheet", "morhetz/gruvbox"];
+  const repos: RepoItemProps[] = [
+    { user: "arsmoriendy", repo: "gruvbox-cheatsheet" },
+    { user: "morhetz", repo: "gruvbox" },
+  ];
 
   return (
     <DropdownMenu placement="bottom-end">
@@ -18,18 +21,27 @@ export const GithubDropdown = () => {
       <DropdownMenuContent class="bg-background/85 backdrop-blur">
         <DropdownMenuLabel>Github Repositories</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <For each={repos}>{(repo) => <RepoItem name={repo} />}</For>
+        <For each={repos}>{(props) => <RepoItem {...props} />}</For>
       </DropdownMenuContent>
     </DropdownMenu>
   );
 };
 
-const RepoItem = (props: { name: string }) => (
-  <DropdownMenuItem
-    as="a"
-    class="cursor-pointer"
-    href={`https://github.com/${props.name}`}
-  >
-    {props.name}
-  </DropdownMenuItem>
-);
+type RepoItemProps = {
+  user: string;
+  repo: string;
+};
+
+const RepoItem = ({ user, repo }: RepoItemProps) => {
+  const repoPath = `${user}/${repo}`;
+
+  return (
+    <DropdownMenuItem
+      as="a"
+      class="cursor-pointer"
+      href={`https://github.com/${repoPath}`}
+    >
+      <span class="font-mono">{repoPath}</span>
+    </DropdownMenuItem>
+  );
+};
