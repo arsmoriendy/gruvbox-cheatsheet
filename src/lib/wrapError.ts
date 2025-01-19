379387ec2300e71be6c8022dc5e169dd27b22401
arsmoriendy <nugrahasteven@gmail.com>
2025-01-19 20:10:47 +0700

    feat: add asyncWrapError function

--- src/lib/wrapError.ts
+++ src/lib/wrapError.ts
@@ -5,3 +5,13 @@ export default <T>(f: () => T): { success: boolean; data?: T; error?: any } => {
     return { success: false, data: undefined, error: e };
   }
 };
+
+export const asyncWrapError = async <T>(
+  f: () => Promise<T>,
+): Promise<{ success: boolean; data?: T; error?: any }> => {
+  try {
+    return { success: true, data: await f() };
+  } catch (e) {
+    return { success: false, data: undefined, error: e };
+  }
+};
