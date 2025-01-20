0db3283eacc520a2d5e3b716e87fc5c732abe8d8
arsmoriendy <nugrahasteven@gmail.com>
2025-01-20 17:14:05 +0700

    feat: alternate dropdown motion animation

--- src/components/fragments/ColorTables.tsx
+++ src/components/fragments/ColorTables.tsx
@@ -1,4 +1,11 @@
-import { For, JSX, onMount, useContext } from "solid-js";
+import {
+  createEffect,
+  createSignal,
+  For,
+  JSX,
+  onMount,
+  useContext,
+} from "solid-js";
 import {
   SettingsContext,
   SettingsSchemaShape,
@@ -11,6 +18,7 @@ import Lightbulb from "lucide-solid/icons/lightbulb";
 import { toaster } from "@kobalte/core";
 import ClipboardCheck from "lucide-solid/icons/clipboard-check";
 import { cn } from "~/lib/utils";
+import { animate, stagger } from "motion";
 
 export const ColorTables = () => {
   const [settings, setSettings] = useContext(SettingsContext);
@@ -123,12 +131,46 @@ const Rows = ({
               ? "hsl(48 87% 88%)"
               : "hsl(0 0% 16%)";
 
+          // hocus: hovered/focused
+          const [hocus, setHocus] = createSignal(false);
+
+          const animateCellsDrop = (cells: NodeListOf<Element>) => {
+            animate(
+              cells,
+              { opacity: [0, 100], y: [-100, 0] },
+              { delay: stagger(0.1) },
+            );
+          };
+
+          let ul!: HTMLUListElement;
+
+          createEffect(() => {
+            hocus() && animateCellsDrop(ul.querySelectorAll("&>div"));
+          });
+
           return (
             <Td class="p-0">
-              <button class="relative w-full text-left group hover:z-40 focus:z-40">
-                <div class="hidden group-hover:block group-focus:block fixed top-0 left-0 w-full h-full bg-background/50 pointer-events-none" />
+              <button
+                class={cn("relative w-full text-left group", hocus() && "z-40")}
+                onmouseenter={() => setHocus(true)}
+                onfocusin={() => setHocus(true)}
+                onmouseleave={() => setHocus(false)}
+                onfocusout={() => setHocus(false)}
+              >
+                <div
+                  class={cn(
+                    "hidden fixed top-0 left-0 w-full h-full bg-background/50 pointer-events-none",
+                    hocus() && "block",
+                  )}
+                />
                 <Cell class="relative" bg={colorStr} fg={textColor} />
-                <div class="absolute w-full hidden group-hover:flex group-focus:flex flex-col gap-2 pt-2">
+                <ul
+                  ref={ul}
+                  class={cn(
+                    "absolute w-full hidden flex-col gap-2 pt-2",
+                    hocus() && "flex",
+                  )}
+                >
                   <For each={otherColorFormats}>
                     {(fmt) => (
                       <Cell
@@ -138,7 +180,7 @@ const Rows = ({
                       />
                     )}
                   </For>
-                </div>
+                </ul>
               </button>
             </Td>
           );
